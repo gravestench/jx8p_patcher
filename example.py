b35aaa5f9cce59045d0668ee0a98bf0452d6b767
Dylan Knuth <dknuth0101@gmail.com>
2015-11-23 09:19:07 -0800

    changed example, messing with different backends

--- example.py
+++ example.py
@@ -1,19 +1,30 @@
 import pygame, mido
 from pygame import midi as midi
-import random as rand
+import random
 import os
+
 
 # for some reason, mido can't connect in my environment unless
 # i use pygame (to initialize portmidi...?)
-midi.init()
+
+rtmidi = mido.Backend('mido.backends.rtmidi')
+portmidi = mido.Backend('mido.backends.portmidi')
+
+import jx8p_patcher as jx8p
 
 # This is my midi controller, which has din-midi and usb-midi
 # the jx8p is connected to this controller
-mo=mido.open_output("Bitstream 3X")
-mi=mido.open_input("Bitstream 3X")
+mo=portmidi.open_output("Bitstream 3x")
+mi=portmidi.open_input("Bitstream 3x")
+
+virtual_name="jx8p_patcher"
+vmo=rtmidi.open_output(virtual_name, virtual=True)
+vmi=rtmidi.open_input(virtual_name, virtual=True)
 
 # This is my jx8p patcher
 import jx8p_patcher as jx8p
+
+midi.init()
 
 # I have a bunch of .syx files for the jx8p
 # here is an example of loading a .syx file as a patch
@@ -46,15 +57,35 @@ def rp():
   """
   p = jx8p.Patch()
   for param in p.parameters:
-    param.value = rand.randint(0,128)
+    param.value = random.randint(0,128)
   return p
 
 def rsyx(dirpath):
   filepaths=os.listdir(dirpath)
   syxfiles=[s for s in filepaths if "SYX" in s]
-  return dirpath + '\\' + rand.choice(syxfiles)
+  return dirpath + '\\' + random.choice(syxfiles)
 
 def print_incoming(input_port):
   while True:
     m=input_port.receive()
     print m
+
+import os, random
+def rsyx(dirpath):
+  filepaths=os.listdir(dirpath)
+  syxfiles=[s for s in filepaths if "SYX" in s]
+  return dirpath + '\\' + random.choice(syxfiles)
+
+def rcclerp():
+  my_sysex_dir = r"C:\Users\Dylan\Downloads\jx8p.syx\jcb"
+  p1=jx8p.Patch(rsyx(my_sysex_dir))
+  p2=jx8p.Patch(rsyx(my_sysex_dir))
+  cclerp(jx8p.Patch(rsyx(my_sysex_dir)),jx8p.Patch(rsyx(my_sysex_dir)))
+
+def cclerp(patch1, patch2):
+  while True:
+    m = mi.receive() # `mi` is a mido input port
+    print m
+    if m.type == 'control_change':
+      p=jx8p.lerp2(patch1, patch2, m.value)
+      mo.send(jx8p.message(p))
